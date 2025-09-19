cb25b642efd74131dd7fc47beb321685726db092
Fadel1106 <muhfadelramadan06@gmail.com>
2025-09-19 10:36:00 +0700

    Update 15 frekuensi karakter.cpp

--- Latihan/15 frekuensi karakter.cpp
+++ Latihan/15 frekuensi karakter.cpp
@@ -7,16 +7,13 @@ int main() {
     string teks = "programming";
     map<char, int> frekuensi;
 
-    // Hitung frekuensi tiap karakter
     for (char c : teks) {
         frekuensi[c]++;
     }
 
-    // Tampilkan hasil
     cout << "Frekuensi karakter:" << endl;
     for (auto pair : frekuensi) {
         cout << "'" << pair.first << "': " << pair.second << endl;
     }
-
     return 0;
 }
