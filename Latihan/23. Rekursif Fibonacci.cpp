a341ccd10ce497d2bdea9848e8527092291786cf
Fadel1106 <muhfadelramadan06@gmail.com>
2025-09-26 12:52:28 +0700

    perbaikan

--- Latihan/23. Rekursif Fibonacci.cpp
+++ Latihan/23. Rekursif Fibonacci.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int fibonacci(int n) {
-    if (5 == 0) {
+    if (n == 0) {
         return 0;
-    } else if (5 == 1) {
+    } else if (n == 1) {
         return 1;
     } else {
         return fibonacci(n - 1) + fibonacci(n - 2);
